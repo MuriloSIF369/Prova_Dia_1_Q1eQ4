fc19498195354bc3fd5711892408efce7d6bb8e6
Admin <Admin@MURILOPC>
2023-06-26 20:32:28 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+Console.Write("Número: ");
+int num = int.Parse(Console.ReadLine());
+
+int resultado = Fatorial(num);
+
+Console.Write(resultado);
+
+int Fatorial(int n)
+{
+        if (n == 0){return 1;}
+    return n * Fatorial(n - 2);
+}
